8225ba84054d0ec4324602a61c58274cd613f5f2
Najc12 <najc12@gmail.com>
2026-05-14 14:22:47 +0200

    Add files via upload

---
+++ Craftea/script.js
@@ -0,0 +1,20 @@
+body {
+  background: #111;
+  color: white;
+  font-family: Arial;
+  text-align: center;
+  margin-top: 100px;
+}
+
+h1 {
+  color: cyan;
+  font-size: 50px;
+}
+
+button {
+  padding: 12px 20px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 18px;
+}
