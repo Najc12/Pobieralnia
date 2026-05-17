07739a38d52b14871e842db2a02098771309cc7a
Najc12 <najc12@gmail.com>
2026-05-17 14:18:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-body {
-  background: #111;
-  color: white;
-  font-family: Arial;
-  text-align: center;
-  margin-top: 100px;
+const toggleButton = document.getElementById("theme-toggle");
+
+/* ZAPIS MOTYWU */
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-theme");
+  toggleButton.textContent = "☀️";
 }
 
-h1 {
-  color: cyan;
-  font-size: 50px;
-}
+/* KLIK */
+toggleButton.addEventListener("click", () => {
 
-button {
-  padding: 12px 20px;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 18px;
-}
+  document.body.classList.toggle("light-theme");
+
+  /* TRYB */
+  if (document.body.classList.contains("light-theme")) {
+
+    toggleButton.textContent = "☀️";
+
+    localStorage.setItem("theme", "light");
+
+  } else {
+
+    toggleButton.textContent = "🌙";
+
+    localStorage.setItem("theme", "dark");
+  }
+
+});
+
+// LIGHTBOX
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".lightbox .close");
+
+// kliknięcie zdjęcia
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+// zamykanie
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
