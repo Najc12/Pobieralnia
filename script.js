0c30b20845aeadbbc39d42fae4ed874524c669f0
Najc12 <najc12@gmail.com>
2026-05-27 18:18:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,16 @@ toggleButton.addEventListener("click", () => {
 
   document.body.classList.toggle("light-theme");
 
-  /* TRYB */
   if (document.body.classList.contains("light-theme")) {
 
     toggleButton.textContent = "☀️";
-
     localStorage.setItem("theme", "light");
 
   } else {
 
     toggleButton.textContent = "🌙";
-
     localStorage.setItem("theme", "dark");
+
   }
 
 });
@@ -32,24 +30,38 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".lightbox .close");
 
-// kliknięcie zdjęcia
-document.querySelectorAll(".gallery img").forEach(img => {
-  img.addEventListener("click", () => {
-    lightbox.style.display = "flex";
-    lightboxImg.src = img.src;
+// tylko jeśli istnieje galeria
+if (lightbox && lightboxImg && closeBtn) {
+
+  document.querySelectorAll(".gallery img").forEach(img => {
+
+    img.addEventListener("click", () => {
+
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
+
+    });
+
   });
-});
 
-// zamykanie
-closeBtn.addEventListener("click", () => {
-  lightbox.style.display = "none";
-});
+  closeBtn.addEventListener("click", () => {
 
-lightbox.addEventListener("click", (e) => {
-  if (e.target === lightbox) {
     lightbox.style.display = "none";
-  }
-});
+
+  });
+
+  lightbox.addEventListener("click", (e) => {
+
+    if (e.target === lightbox) {
+
+      lightbox.style.display = "none";
+
+    }
+
+  });
+
+}
+
 // AKTUALNOŚCI
 document.querySelectorAll(".news-button").forEach(button => {
 
@@ -60,11 +72,52 @@ document.querySelectorAll(".news-button").forEach(button => {
     card.classList.toggle("active");
 
     if (card.classList.contains("active")) {
+
       button.textContent = "Ukryj";
+
     } else {
+
       button.textContent = "Czytaj więcej";
+
     }
 
   });
 
 });
+// REKRUTACJA
+document.querySelectorAll(".recruit-toggle").forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    const card = button.parentElement;
+
+    card.classList.toggle("active");
+
+    if (card.classList.contains("active")) {
+
+      button.textContent = "Ukryj";
+
+    } else {
+
+      button.textContent = "Czytaj więcej";
+
+    }
+
+  });
+
+});
+// ENTER W OGLĄDANKO
+const passwordInput = document.getElementById("watch-password");
+const loginButton = document.getElementById("watch-login-button");
+
+if (passwordInput && loginButton) {
+
+  passwordInput.addEventListener("keypress", (e) => {
+
+    if (e.key === "Enter") {
+      loginButton.click();
+    }
+
+  });
+
+}
